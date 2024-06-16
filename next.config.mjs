d34c5a954975b5838ee1597f4137f82002bfdfeb
ARP-25 <angelo.pucci@outlook.de>
2024-06-16 11:53:26 +0000

    prod config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,28 @@
-/** @type {import('next').NextConfig} */
+import { config } from 'dotenv';
+
+config({ path: '.env.prod' });
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: "8000",
-                pathname: "/**",
-            },
-            {
-                protocol: "http",
-                hostname: "164.90.164.114",
-                port: "1337",
-                pathname: "/**",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "164.90.164.114",
+        port: "1337",
+        pathname: "/**",
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_API_HOST: process.env.NEXT_PUBLIC_API_HOST,
+    NEXT_PUBLIC_WS_HOST: process.env.NEXT_PUBLIC_WS_HOST,
+  },
 };
 
 export default nextConfig;
